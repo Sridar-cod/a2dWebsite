b1cf115efa1b5dead1eaa59ac9b56140f1b8aac0
Sridar-cod <sridar9771@gmail.com>
2024-10-01 12:38:30 +0530

    update mongo url

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,3 +1,9 @@
 import mongoose from "mongoose";
 
-export  const  connectMongo = async () => mongoose.connect(process.env.MONGO_URL)
+// export  const  connectMongo = async () => mongoose.connect(process.env.MONGO_URL)
+export const connectMongo = async () => {
+  return(
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err)))
+}
