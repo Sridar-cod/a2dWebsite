50ef4e1e0ce7a6ddecde9f1d8085774eef90d510
Sridar-cod <sridar9771@gmail.com>
2024-10-01 13:33:36 +0530

    updated mongoDb

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,30 @@
 "use client"; // Add this at the top to mark the file as a Client Component
 
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 const formFields = [
   { label: "What's your name?", name: "name", type: "text" },
   { label: "What's your exact age?", name: "age", type: "number" },
   { label: "Provide your WhatsApp number", name: "whatsapp", type: "text" },
-  { label: "Where are you located at?", name: "location", type: "text", placeholder: "Mention the City or Town" },
-  { label: "What do you do for living?", name: "occupation", type: "text", placeholder: "E.g. Developer, Student, Photographer etc." },
-  { label: "For what purpose does this PC serve you?", name: "purpose", type: "text", placeholder: "E.g. Video Editing, Gaming etc." },
+  {
+    label: "Where are you located at?",
+    name: "location",
+    type: "text",
+    placeholder: "Mention the City or Town",
+  },
+  {
+    label: "What do you do for living?",
+    name: "occupation",
+    type: "text",
+    placeholder: "E.g. Developer, Student, Photographer etc.",
+  },
+  {
+    label: "For what purpose does this PC serve you?",
+    name: "purpose",
+    type: "text",
+    placeholder: "E.g. Video Editing, Gaming etc.",
+  },
   {
     label: "The best investment you are planning for this PC?",
     name: "investment",
@@ -19,52 +34,52 @@ const formFields = [
       "Rs 50,000 - Rs 75,000",
       "Rs 75,000 - Rs 100,000",
       "More than 1 Lakh",
-      "Other"
-    ]
+      "Other",
+    ],
   },
   {
     label: "When do you need the PC by?",
     name: "timeline",
     type: "select",
-    options: ["Right now", "In few days", "In a month", "Other"]
+    options: ["Right now", "In few days", "In a month", "Other"],
   },
   {
     label: "What is your monthly income?",
     name: "income",
     type: "text",
-    placeholder: "*This will help us suggest a best Investment for you*"
+    placeholder: "*This will help us suggest a best Investment for you*",
   },
   {
-    label: "If you have any suggestions or recommendations regarding this PC build, kindly mention it in detail.",
+    label:
+      "If you have any suggestions or recommendations regarding this PC build, kindly mention it in detail.",
     name: "suggestions",
-    type: "textarea"
-  }
+    type: "textarea",
+  },
 ];
 
 export default function Home() {
-  // const [enquiries, setEnquiries] = useState([]);
+  const [enquiries, setEnquiries] = useState([]);
 
-  // useEffect(() => {
-  //   // Function to fetch data from the API
-  //   const fetchEnquiries = async () => {
-  //     try {
-  //       const response = await fetch("http://localhost:3000/api/getEnquiry");
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch enquiries");
-  //       }
-  //       const data = await response.json(); // Assuming the API returns a JSON response
-  //       console.log(await data)
-  //       setEnquiries(data); // Update state with the fetched data
-  //     } catch (error) {
-  //       console.error("Error fetching enquiriessss:", error);
-  //     }
-  //   };
+  useEffect(() => {
+    // Function to fetch data from the API
+    const fetchEnquiries = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/getEnquiry");
+        if (!response.ok) {
+          throw new Error("Failed to fetch enquiriessssss");
+        }
+        const data = await response.json();
+        console.log(await data, "dataaaa");
+        setEnquiries(data);
+      } catch (error) {
+        console.error("Error fetching enquiriessss:", error);
+      }
+    };
 
-  //   // Call the fetch function
-  //   fetchEnquiries();
-  // }, []); 
+    // Call the fetch function
+    fetchEnquiries();
+  }, []);
   const [formData, setFormData] = useState({});
-
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -76,8 +91,9 @@ export default function Home() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData)
+    console.log(formData);
     fetch("https://a2dwebsite.onrender.com/api/enquiry", {
+    // fetch("http://localhost:3000/api/enquiry", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -98,11 +114,17 @@ export default function Home() {
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-black">
-      <form onSubmit={handleSubmit} className="p-4 mt-5 mb-5 bg-white rounded shadow" style={{ maxWidth: "500px", width: "100%" }}>
+      <form
+        onSubmit={handleSubmit}
+        className="p-4 mt-5 mb-5 bg-white rounded shadow"
+        style={{ maxWidth: "500px", width: "100%" }}
+      >
         <h2 className="mb-4 text-center">Hii Soldiers</h2>
         {formFields.map((field, index) => (
           <div key={index} className="mb-3">
-            <label htmlFor={field.name} className="form-label">{field.label}</label>
+            <label htmlFor={field.name} className="form-label">
+              {field.label}
+            </label>
             {field.type === "select" ? (
               <select
                 name={field.name}
@@ -137,17 +159,18 @@ export default function Home() {
             )}
           </div>
         ))}
-        <button type="submit" className="btn btn-primary w-100">Submit</button>
+        <button type="submit" className="btn btn-primary w-100">
+          Submit
+        </button>
       </form>
       {/* <div className="">
         <h1>data</h1>
-        {enquiries.map((item) => {
+        {enquiries?.map((item) => {
           return (
             <li>{item.name}</li>
           )
         })}
       </div> */}
-     
     </div>
   );
 }
--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-// export  const  connectMongo = async () => mongoose.connect(process.env.MONGO_URL)
-export const connectMongo = async () => {
-  return(
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.error("MongoDB connection error:", err)))
-}
+ export  const  connectMongo = async () => await mongoose.connect(process.env.MONGO_URI)
+// export const connectMongo = async () => {
+//   return(
+//   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log("MongoDB connected"))
+//     .catch(err => console.error("MongoDB connection error:", err)))
+// }
