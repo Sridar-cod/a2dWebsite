26f4c2a053be39606def4c852ecaa6751715f45a
Sridar-cod <sridar9771@gmail.com>
2024-10-04 14:29:12 +0530

    .

--- src/app/page.js
+++ src/app/page.js
@@ -157,9 +157,9 @@ export default function Home() {
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title" id="modalTitle">Form Submission</h5>
-              <button type="button" className="close" onClick={handleModalClose}>
+              {/* <button type="button" className="close" onClick={handleModalClose}>
                 <span>&times;</span>
-              </button>
+              </button> */}
             </div>
             <div className="modal-body">
               <p>{modalMessage}</p>
